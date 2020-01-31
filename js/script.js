7f106c68906e1f050539ffd6b3eb080d4c80ae5d
bhavyap2 <bhavyap281998@gmail.com>
2020-01-31 12:22:14 +0530

    hhh

---
+++ js/script.js
@@ -0,0 +1,240 @@
+// $(document).ready(function () {
+
+
+
+	
+;(function ($) {
+	'use strict';
+
+  $(window).scroll(function () {
+    if ($('.navigation').offset().top > 100) {
+      $('.navigation').addClass('fixed-nav');
+    } else {
+      $('.navigation').removeClass('fixed-nav');
+    }
+  });
+  
+
+ $('.portfolio-gallery').each(function () {
+        $(this).find('.popup-gallery').magnificPopup({
+            type: 'image',
+            gallery: {
+                enabled: true
+            }
+        });
+    });
+
+
+	$('#contact-form').validate({
+		rules: {
+			user_name: {
+				required: true,
+				minlength: 4
+			},
+			user_email: {
+				required: true,
+				email: true
+			},
+			// user_subject: {
+			// 	required: false
+			// },
+			user_message: {
+				required: true
+			}
+		},
+		messages: {
+			user_name: {
+				required: 'Come on, you have a name don\'t you?',
+				minlength: 'Your name must consist of at least 2 characters'
+			},
+			user_email: {
+				required: 'Please put your email address'
+			},
+			user_message: {
+				required: 'Put some messages here?',
+				minlength: 'Your name must consist of at least 2 characters'
+			}
+
+		},
+		submitHandler: function (form) {
+			$(form).ajaxSubmit({
+				type: 'POST',
+				data: $(form).serialize(),
+				url: 'sendmail.php',
+				success: function () {
+					$('#contact-form #success').fadeIn();
+				},
+				error: function () {
+
+					$('#contact-form #error').fadeIn();
+				}
+			});
+		}
+	});
+
+
+
+	$('.testimonial-slider').slick({
+		slidesToShow: 1,
+		infinite: true,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		dots:true
+	});
+
+
+
+
+	// Init Magnific Popup
+	$('.portfolio-popup').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		gallery: {
+			enabled: true
+		},
+		mainClass: 'mfp-with-zoom',
+		navigateByImgClick: true,
+		arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>',
+		tPrev: 'Previous (Left arrow key)',
+		tNext: 'Next (Right arrow key)',
+		tCounter: '<span class="mfp-counter">%curr% of %total%</span>',
+		zoom: {
+			enabled: true,
+			duration: 300,
+			easing: 'ease-in-out',
+			opener: function (openerElement) {
+				return openerElement.is('img') ? openerElement : openerElement.find('img');
+			}
+		}
+	});
+
+
+
+
+	var map;
+
+	function initialize() {
+		var mapOptions = {
+			zoom: 13,
+			center: new google.maps.LatLng(50.97797382271958, -114.107718560791)
+			// styles: style_array_here
+		};
+		map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	}
+
+	var google_map_canvas = $('#map-canvas');
+
+	if (google_map_canvas.length) {
+		google.maps.event.addDomListener(window, 'load', initialize);
+	}
+
+
+
+
+})(jQuery);
+
+
+
+// stats counter
+
+(function ($) {
+	$.fn.countTo = function (options) {
+		options = options || {};
+		
+		return $(this).each(function () {
+			// set options for current element
+			var settings = $.extend({}, $.fn.countTo.defaults, {
+				from:            $(this).data('from'),
+				to:              $(this).data('to'),
+				speed:           $(this).data('speed'),
+				refreshInterval: $(this).data('refresh-interval'),
+				decimals:        $(this).data('decimals')
+			}, options);
+			
+			// how many times to update the value, and how much to increment the value on each update
+			var loops = Math.ceil(settings.speed / settings.refreshInterval),
+				increment = (settings.to - settings.from) / loops;
+			
+			// references & variables that will change with each update
+			var self = this,
+				$self = $(this),
+				loopCount = 0,
+				value = settings.from,
+				data = $self.data('countTo') || {};
+			
+			$self.data('countTo', data);
+			
+			// if an existing interval can be found, clear it first
+			if (data.interval) {
+				clearInterval(data.interval);
+			}
+			data.interval = setInterval(updateTimer, settings.refreshInterval);
+			
+			// initialize the element with the starting value
+			render(value);
+			
+			function updateTimer() {
+				value += increment;
+				loopCount++;
+				
+				render(value);
+				
+				if (typeof(settings.onUpdate) == 'function') {
+					settings.onUpdate.call(self, value);
+				}
+				
+				if (loopCount >= loops) {
+					// remove the interval
+					$self.removeData('countTo');
+					clearInterval(data.interval);
+					value = settings.to;
+					
+					if (typeof(settings.onComplete) == 'function') {
+						settings.onComplete.call(self, value);
+					}
+				}
+			}
+			
+			function render(value) {
+				var formattedValue = settings.formatter.call(self, value, settings);
+				$self.html(formattedValue);
+			}
+		});
+	};
+	
+	$.fn.countTo.defaults = {
+		from: 0,               // the number the element should start at
+		to: 0,                 // the number the element should end at
+		speed: 1000,           // how long it should take to count between the target numbers
+		refreshInterval: 100,  // how often the element should be updated
+		decimals: 0,           // the number of decimal places to show
+		formatter: formatter,  // handler for formatting the value before rendering
+		onUpdate: null,        // callback method for every time the element is updated
+		onComplete: null       // callback method for when the element finishes updating
+	};
+	
+	function formatter(value, settings) {
+		return value.toFixed(settings.decimals);
+	}
+}(jQuery));
+
+jQuery(function ($) {
+  // custom formatting example
+  $('.count-number').data('countToOptions', {
+	formatter: function (value, options) {
+	  return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
+	}
+  });
+  
+  // start all the timers
+  $('.timer').each(count);  
+  
+  function count(options) {
+	var $this = $(this);
+	options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+	$this.countTo(options);
+  }
+});
+
+// end //
